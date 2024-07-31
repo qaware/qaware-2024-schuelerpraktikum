7e85c3ca81c1b80ebd14d624e407ceb0907fe9af
mihail.degas <mihail.degas@qaware.de>
2024-07-31 11:53:54 +0200

    Add sensor names

--- Benutzer.py
+++ Benutzer.py
@@ -27,13 +27,13 @@ class Animation(object):
         py.draw.rect(fenster, self.colors.white, [self.display.getScreenWidth()*0.6, self.display.getScreenHeight()*0.35,self.display.getScreenWidth()*0.35, (self.display.getScreenHeight()*0.61)], border_radius=10)
 
         self.addHeadlines(data_typen,fenster)
+        self.addNamen(data,fenster)
 
     def sensorKontrollScreen_Start_dynamisch(self,data):
         fenster = py.display.set_mode((self.display.getScreenWidth(),self.display.getScreenHeight()))
         py.display.set_caption("Satellitenkontrollprogramm")
         clock = py.time.Clock()
-        data_typen = getAnzahlTypenausDaten(data)
-        print(data_typen)
+        data_typen = getTypenausDaten(data)
         self.sensorKnotrollscreen_Start(fenster,data_typen)
         while True:
             for event in py.event.get():
@@ -49,22 +49,38 @@ class Animation(object):
         fenster.blit(self.fonts.font.render("alle historische Daten", True, self.colors.light_grey), (self.display.getScreenWidth()*0.61,self.display.getScreenHeight()*0.25))
         fenster.blit(self.fonts.font.render("letzten Daten", True, self.colors.light_grey), (self.display.getScreenWidth()*0.23,self.display.getScreenHeight()*0.67))
         for index,sensorArt in enumerate(data_typen):
-            fenster.blit(self.fonts.small_font.render(sensorArt, True, self.colors.light_grey), (self.display.getScreenWidth()*0.07,self.display.getScreenHeight()*0.06+self.display.getScreenHeight()*(0.95/len(data_typen))*index))
+            fenster.blit(self.fonts.small_font.render(sensorArt, True, self.colors.blackish), (self.display.getScreenWidth()*0.07,self.display.getScreenHeight()*0.06+self.display.getScreenHeight()*(0.95/len(data_typen))*index))
+
+    def addNamen(self,data,fenster):
+        sensoren_namen = getSensorenNamen(data)
+        for sensor_typ_index,sensorTyp in enumerate(sensoren_namen):
+            for sensor_name_index,sensorName in enumerate(sensoren_namen[sensorTyp]):
+                fenster.blit(self.fonts.small_font.render(sensorName, True, self.colors.light_grey), (self.display.getScreenWidth()*0.07,self.display.getScreenHeight()*0.11+self.display.getScreenHeight()*(0.95/len(sensoren_namen))*sensor_typ_index+35*sensor_name_index))
 
 def datenopen():
     for x in open("Example_data.json","r"):
         print(x)
 
-def getAnzahlTypenausDaten(data):
+def getTypenausDaten(data):
     data_typen = []
     for messung in data:
-        if data[messung]["type"] not in data_typen:
-            data_typen += [data[messung]["type"]]
+        if data[messung]["data_type"] not in data_typen:
+            data_typen += [data[messung]["data_type"]]
     return data_typen
+
+def getSensorenNamen(data):
+    sensoren_namen = {}
+    for messung in data:
+        if data[messung]["data_type"] not in sensoren_namen.keys():
+            sensoren_namen[data[messung]["data_type"]] = [data[messung]["name"]]
+        else:
+            sensoren_namen[data[messung]["data_type"]] += [data[messung]["name"]]
+    return sensoren_namen
+
 
 if __name__ == '__main__':
     py.init()
-    data = {"data": {"type": "thruster", "name": "thruster_3.c", "val": 8.703344683253974, "time": 215.2221422943402},"data2":{"type": "temperatur", "name": "thruster_3.c", "val": 8.703344683253974, "time": 215.2221422943402},"data3":{"type": "Höhe", "name": "thruster_3.c", "val": 8.703344683253974, "time": 215.2221422943402}}
+    data = {"data": {"data_type": "thruster", "name": "thruster_3.c", "val": 8.703344683253974, "time": 215.2221422943402},"data2":{"data_type": "temperatur", "name": "thruster_3.c", "val": 8.703344683253974, "time": 215.2221422943402},"data3":{"data_type": "temperatur", "name": "thruster_3.c", "val": 8.703344683253974, "time": 215.2221422943402}}
     screen = Screen()
     colors = Colors()
     fonts = Fonts()
