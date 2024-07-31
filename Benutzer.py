f264f1b0813b2eb0ea05a78a338645d5a2a8d919
janus.kracke <janus.kracke@qaware.de>
2024-07-31 10:20:48 +0200

    revert changes to Benutzer.py

--- Benutzer.py
+++ Benutzer.py
@@ -1,10 +1,8 @@
 import json
-import time
-URL = "http://127.0.0.1:8000"
 
 import requests
 
-from models import SensorData, UpdateDataModel
+from models import Sensor_data, UpdateDataModel
 
 # TODO: Wie erhalten wir für uns interessante Informationen von der Verwaltung?
 # TODO: Was sind interessante Daten und welche benötigen wir unter Umständen gar nicht?
@@ -12,11 +10,13 @@ from models import SensorData, UpdateDataModel
 # TODO: Wie stellen wir die Informationen bestmöglich für den Nutzer da?
 
 if __name__ == '__main__':
-    sens_data = SensorData(value=1.10, name="Test", data_type="bla", time=123)
-    sens_data2 = SensorData(value=1.10, name="Test", data_type="bla", time=123)
-    answer1 = requests.post("http://127.0.0.1:8000/data/addData/", sens_data.json())
-    answer2 = requests.get(f"{URL}/data/doesExist/{sens_data2.data_type}/{sens_data2.name}/{sens_data2.time}")
+    data = Sensor_data(name="Test")
+    new_data = UpdateDataModel(name="Updated Test")
+    answer1 = requests.post("http://127.0.0.1:8000/data/", data.json())
+    answer2 = requests.put(f"http://127.0.0.1:8000/data/{json.loads(answer1.content)['_id']}", new_data.json())
+    answer3 = requests.delete(f"http://127.0.0.1:8000/data/{json.loads(answer1.content)['_id']}")
+    answer4 = requests.get(f"http://127.0.0.1:8000/data/{json.loads(answer1.content)['_id']}")
     print(answer1.content)
     print(answer2.content)
-    answer3 = requests.get(f"{URL}/data/get/bla")
     print(answer3.content)
+    print(answer4.content)
