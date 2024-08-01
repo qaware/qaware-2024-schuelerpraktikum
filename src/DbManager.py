cb3edbc6bf0a858b3907b525ab636a5b9d8912e1
lev.soltys <lev.soltys@qaware.de>
2024-08-01 13:27:45 +0200

    feat: saving completed

--- src/DbManager.py
+++ src/DbManager.py
@@ -33,34 +33,43 @@ async def return_db():
 
     return JSONResponse(status_code=status.HTTP_200_OK, content=latest_data)
 
+
 async def get_db():
     latest_content = await get_latest_db_contents()
     latest_data = latest_content["data"]
     return latest_data
 
+
 # @app.get("/get-id", response_description="Returned database dict id")
 async def get_latest_db_id():
     latest_content = await get_latest_db_contents()
     latest_id = latest_content["_id"]
-    # latest_id_str = str(latest_id)
-    # print(" id:", latest_id, type(latest_id), latest_id_str)
 
     # return JSONResponse(status_code=status.HTTP_200_OK, content=latest_id_str)
     return latest_id
 
 
-@app.post("/save", response_description="Initial")
+@app.post("/save", response_description="Save db")
 async def save_db(new_data):
-
     # print("save db executed")
-    # latest_id_request = requests.get("http://127.0.0.1:8000/get-id")
     latest_id = await get_latest_db_id()
-    # print(latest_id)
-    # latest_id = latest_id_request.content
+    print(latest_id)
     print("latest id", latest_id)
     await db["data"].update_one({"_id": latest_id}, {"$set": jsonable_encoder({"data": new_data})})
     print("Updated")
     return JSONResponse(status_code=status.HTTP_200_OK, content="")
+
+
+def get_current_sensor_db(current_db, sensor_type, sensor_name):
+    if sensor_type not in current_db:
+        current_db[sensor_type] = {}
+
+    if sensor_name not in current_db:
+        current_db[sensor_type][sensor_name] = []
+
+    current_sensor_db = current_db[sensor_type][sensor_name]
+
+    return current_sensor_db
 
 
 @app.post("/append-to-db", response_description="Returned database dict")
@@ -76,7 +85,8 @@ async def append_to_db():
     print("Appending")
     current_db = await get_db()
     print("Mapping", current_db)
-    current_sensor_db = current_db[new_raw_data["type"]][new_raw_data["name"]]
+
+    current_sensor_db = get_current_sensor_db(current_db, new_raw_data["type"], new_raw_data["name"])
 
     new_sensor_entry = {
         "pressure": new_raw_data["pressure"],
@@ -91,7 +101,6 @@ async def append_to_db():
 
 @app.post("/create", response_description="Initial")
 async def create_db():
-
     initial_data = {"data": {}}
     json = jsonable_encoder(initial_data)
     await db["data"].insert_one(json)
@@ -99,7 +108,6 @@ async def create_db():
 
 
 if __name__ == '__main__':
-    # resp1 = requests.post("http://127.0.0.1:8000/create")
     resp1 = requests.post("http://127.0.0.1:8000/append-to-db")
     resp1 = requests.get("http://127.0.0.1:8000/return-db")
     print(resp1.status_code, resp1.content)
