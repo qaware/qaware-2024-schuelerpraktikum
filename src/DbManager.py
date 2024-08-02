af015ad374168325e910710b9359bcd7407b5d7f
lev.soltys <lev.soltys@qaware.de>
2024-08-02 09:27:43 +0200

    feat: database done

--- src/DbManager.py
+++ src/DbManager.py
@@ -11,8 +11,8 @@ from starlette.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from bson import ObjectId
 from pydantic.class_validators import Optional
-from ModelHandling import ModelMapper
-from ModelHandling import SensorDataModel
+from src.ModelHandling import ModelMapper
+from src.ModelHandling import SensorDataModel
 
 app = FastAPI()
 mapper = ModelMapper()
@@ -46,6 +46,9 @@ async def return_db():
 
 async def get_db():
     latest_content = await get_latest_db_contents()
+    if latest_content == {}:
+        return {}
+
     latest_data = latest_content["data"]
     return latest_data
 
@@ -77,37 +80,52 @@ async def save_db(new_data):
 def get_current_sensor_db(current_db, sensor_type, sensor_name):
     if sensor_type not in current_db:
         current_db[sensor_type] = {}
+        print("Type doesn't exist")
 
-    if sensor_name not in current_db:
+    if sensor_name not in current_db[sensor_type]:
         current_db[sensor_type][sensor_name] = []
+        print("Sensor doesn't exist")
 
     current_sensor_db = current_db[sensor_type][sensor_name]
 
     return current_sensor_db
 
 
+async def db_is_empty():
+    db = await get_db()
+    return db == {}
+
 @app.post("/append-to-db", response_description="Returned database dict")
-async def append_to_db(new_raw_data: SensorDataModel):
-    print("Appending", new_raw_data)
+async def append_to_db(new_raw_data_model: SensorDataModel):
+    if await db_is_empty():
+        print("Creating database")
+        await create_db()
+    else:
+        print(" DB not empty")
+
+    print("Appending", new_raw_data_model)
+    new_raw_data = mapper.model_to_dict(new_raw_data_model)
 
     current_db = await get_db()
     print("Mapping", current_db)
 
     current_sensor_db = get_current_sensor_db(current_db, new_raw_data["type"], new_raw_data["name"])
-
+    print(current_sensor_db)
     new_sensor_entry = {
         "pressure": new_raw_data["pressure"],
         "temperature": new_raw_data["temperature"],
         "timestamp": new_raw_data["timestamp"]
     }
-
     current_sensor_db.append(new_sensor_entry)
+    # current_db[new_raw_data["type"]][new_raw_data["name"]] = current_sensor_db
 
+    print(current_db)
     await save(current_db)
 
 
 @app.post("/create", response_description="Initial")
 async def create_db():
+    print("Creating db")
     initial_data = {"data": {}}
     json = jsonable_encoder(initial_data)
     await db["data"].insert_one(json)
@@ -117,18 +135,15 @@ async def create_db():
 if __name__ == '__main__':
     data = {
         "name": "AA",
-        "type": "AAA",
-        "pressure": 12.2,
-        "temperature": 231.122,
-        "timestamp": "10001"
+        "type": "AA2224",
+        "pressure": 12.5,
+        "temperature": 0,
+        "timestamp": "10001236"
     }
 
     data = mapper.dict_to_model(data)
-    print(data)
 
-    # resp1 = requests.post("http://127.0.0.1:8000/append-to-db", data.json()) # json.dumps(data), headers={"Content-type": "application/json"}
-    # print(resp1.status_code, resp1.content)
-    #
+    resp1 = requests.post("http://127.0.0.1:8000/append-to-db", data.json())
     # resp1 = requests.get("http://127.0.0.1:8000/return-db")
 
-    # print(resp1.status_code, resp1.content)
+    print(resp1.status_code, resp1.content)
