1ad0dc91f8a64c36b4a700c674966d7cabf490a1
Thea Schneider <thea.schneider@qaware.de>
2024-08-02 10:42:56 +0200

    OUTPUT produziert eine Tabelle aus angelieferten Daten

--- src/OUTPUT.py
+++ src/OUTPUT.py
@@ -1,12 +1,13 @@
 # TODO Warnung bei kritischen Werten
 
-import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse
 from starlette import status
+
+PATH = "./images/"
+FILENAME = "Output.png"
 
 app = FastAPI()
 
@@ -21,78 +22,34 @@ async def create_data(data: dict):
 
 def update_table(data: dict):
 
-    item={}
-    data={
-        "thruster": {
-            "thruster_3.b": [
-                {
-                    "pressure": 0.222,
-                    "temperature": 0.22,
-                    "timestamp": 12887729
-                },
-                {
-                    "pressure": 0.2322,
-                    "temperature": 0.32,
-                    "timestamp": 12887398
-                }
-            ],
-            "thruster_3.a": [
-                {
-                    "pressure": 0.2322,
-                    "temperature": 0.32,
-                    "timestamp": 12887398
-                }
+    # Transformiere Daten
+    table_data = []
+    for sensor_type in data.keys():
+        for name in data[sensor_type].keys():
+            data_list = data[sensor_type][name]
+            item = data_list[-1]
+
+            row = [
+                sensor_type,
+                name,
+                item["pressure"],
+                item["temperature"],
             ]
-        },
-        "gas_valve": {
-            "gas_valve1":[
-                {
-                    "pressure": 0.322,
-                    "temperature": 0.33,
-                    "timestamp": 13333333
-                },
-                {
-                    "pressure": 0.666,
-                    "temperature": 0.66,
-                    "timestamp": 16666668
-                }
-            ]
-        }
-    }
+            table_data.append(row)
+
+    # Daten in einen DataFrame umwandeln und transponieren
+    df = pd.DataFrame(table_data, columns=["Sensor Type", "Sensor Name", "Pressure", "Temperature"])
+    df = df.T
+
+    # Erstelle Tabellen Plot
     fig, ax = plt.subplots()
     rowLabels=["Sensor Type", "Sensor Name","Temperature","Pressure"]
 
     ax.xaxis.set_visible(False)
     ax.yaxis.set_visible(False)
     ax.set_frame_on(False)
-    # TODO: erstelle tabelle
 
-    df2=pd.DataFrame()
-    item_number=0
-    for sensor_type in data.keys():
-        item["type"] = sensor_type
-        for name in data[sensor_type].keys():
-            item["name"] = name
-            data_list = data[sensor_type][name]
-            item["pressure"] = data_list[-1]["pressure"]
-            item["temperature"]=data_list[-1]["temperature"]
-            item["timestamp"]=data_list[-1]["timestamp"]
-            df=pd.DataFrame({'type': item["type"],
-                             'name': item["name"],
-                             'pressure': item["pressure"],
-                             'temperature':item["temperature"],
-                             },
-                            index=[item["timestamp"]])
-            df2 = df2 + df
-            print(df)
-            item_number=item_number+1
-
-            # TODO: schreibe neues item in tabelle rein
-
-    df2=df2.T
-
-    print(df2.values)
-    table = ax.table(cellText=df2,
+    table = ax.table(cellText=df.values,
                      rowLabels=rowLabels,
                      cellLoc='center',
                      loc='center')
@@ -111,7 +68,45 @@ def update_table(data: dict):
 
     plt.subplots_adjust(left=0.5, top=0.8)
 
-    plt.show()
+    # Speichern der Tabelle in eine Datei
+    plt.savefig(PATH + FILENAME, bbox_inches='tight', dpi=600)
 
-data={}
-update_table(data)
+# Zum testen
+# data={
+#     "thruster": {
+#         "thruster_3.b": [
+#             {
+#                 "pressure": 0.222,
+#                 "temperature": 0.22,
+#                 "timestamp": 12887729
+#             },
+#             {
+#                 "pressure": 0.2322,
+#                 "temperature": 0.32,
+#                 "timestamp": 12887398
+#             }
+#         ],
+#         "thruster_3.a": [
+#             {
+#                 "pressure": 0.2322,
+#                 "temperature": 0.32,
+#                 "timestamp": 12887398
+#             }
+#         ]
+#     },
+#     "gas_valve": {
+#         "gas_valve1":[
+#             {
+#                 "pressure": 0.322,
+#                 "temperature": 0.33,
+#                 "timestamp": 13333333
+#             },
+#             {
+#                 "pressure": 0.666,
+#                 "temperature": 0.66,
+#                 "timestamp": 16666668
+#             }
+#         ]
+#     }
+# }
+# update_table(data)
